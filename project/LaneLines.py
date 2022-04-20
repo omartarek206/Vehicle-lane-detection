9a9b19728366494ae45cf9cf3a9618616f535f89
omarelshamy12 <72103983+omarelshamy12@users.noreply.github.com>
2022-04-20 22:26:35 +0200

    LaneLines.py (till extract_features)

---
+++ project/LaneLines.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+
+def hist(img):
+    # function that draws the histogram
+    # draws the histogram at the bottom half of the image
+    bottom_half = img[img.shape[0]//2:,:]
+    return np.sum(bottom_half, axis=0)
+
+class LaneLines:
+
+    def __init__(self):
+        self.left_fit = None
+        self.right_fit = None
+        self.binary = None
+        self.nonzero = None
+        self.nonzerox = None
+        self.nonzeroy = None
+        self.clear_visibility = True
+        self.dir = []
+        self.left_curve_img = mpimg.imread('left_turn.png')
+        self.right_curve_img = mpimg.imread('right_turn.png')
+        self.keep_straight_img = mpimg.imread('straight.png')
+        self.left_curve_img = cv2.normalize(src=self.left_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.right_curve_img = cv2.normalize(src=self.right_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.keep_straight_img = cv2.normalize(src=self.keep_straight_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.nwindows = 9
+        # Width of the the windows +/- margin
+        self.margin = 100
+        # Mininum number of pixels found to recenter window
+        self.minpix = 50
+
+    def forward(self, img):
+
+        self.extract_features(img)
+        return self.fit_poly(img)
+
+    def pixels_in_window(self, center, margin, height):
+
+        topleft = (center[0]-margin, center[1]-height//2)
+        bottomright = (center[0]+margin, center[1]+height//2)
+
+        condx = (topleft[0] <= self.nonzerox) & (self.nonzerox <= bottomright[0])
+        condy = (topleft[1] <= self.nonzeroy) & (self.nonzeroy <= bottomright[1])
+        return self.nonzerox[condx&condy], self.nonzeroy[condx&condy]
+
+    def extract_features(self, img):
+
+        self.img = img
+        # Height of of windows - based on nwindows and image shape
+        self.window_height = np.int(img.shape[0]//self.nwindows)
+
+        # Identify the x and y positions of all nonzero pixel in the image
+        self.nonzero = img.nonzero()
+        self.nonzerox = np.array(self.nonzero[1])
+        self.nonzeroy = np.array(self.nonzero[0])
