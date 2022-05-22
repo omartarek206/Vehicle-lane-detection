2e6b9720c6efb8039325deace2e30564f8c6976b
fatema-jamal <73313738+fatema-jamal@users.noreply.github.com>
2022-05-22 22:55:57 +0200

    Update yolo_video.py

--- yolo_video.py
+++ yolo_video.py
@@ -32,6 +32,39 @@ parser.add_argument("-t", "--threshold", type=float, default=0.4,
                     help="non-maximum supression threshold")
 parser.add_argument("-y", "--yolo", type=str, default="yolov4-tiny",
                     help="path to yolo directory")
+CONFIDENCE_THRESHOLD = args.confidence
+NMS_THRESHOLD = args.threshold
+if not Path(args.input).exists():
+    raise FileNotFoundError("Path to video file is not exist.")
+
+vc = cv2.VideoCapture(args.input)
+fps=vc.get(cv2.CAP_PROP_FPS)
+if args.yolo=="yolov4-tiny":
+    weights ="yolo/yolov4-tiny.weights"
+    labels ="yolo/labels.txt"
+    cfg ="yolo/yolov4-tiny.cfg"
+elif args.yolo=="yolov4":
+    weights ="yolov4/yolov4.weights"
+    labels ="yolov4/labels.txt"
+    cfg ="yolov4/yolov4.cfg"
+
+
+logger.info("Using {} weights ,{} configs and {} labels.".format(weights, cfg, labels))
+
+class_names = list()
+with open(labels, "r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
+
+COLORS = np.random.randint(0, 255, size=(len(class_names), 3), dtype="uint8")
+
+net = cv2.dnn.readNetFromDarknet(cfg, weights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+layer = net.getLayerNames()
+layer = [layer[i - 1] for i in net.getUnconnectedOutLayers()]
+writer = None
+
                     
 args = parser.parse_args()
 logger.info("Parsed Arguments")
