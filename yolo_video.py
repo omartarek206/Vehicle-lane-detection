540083619329cb88ca418bc450ab05ce9daa485e
3aly <77078308+3aly@users.noreply.github.com>
2022-05-22 22:54:41 +0200

    Update yolo_video.py

--- yolo_video.py
+++ yolo_video.py
@@ -1,1 +1,38 @@
 
+import argparse
+import glob
+import time
+import logging
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s-%(name)s-%(message)s")
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", type=str, default="",
+                    help="path to input video file")
+parser.add_argument("-o", "--output", type=str, default="",
+                    help="path to (optional) output video file")
+parser.add_argument("-d", "--display", type=int, default=1,
+                    help="display output or not (1/0)")
+parser.add_argument("-ht", "--height", type=int, default=1200,
+                    help="height of output")
+parser.add_argument("-wt", "--width", type=int, default=700,
+                    help="width of output")
+parser.add_argument("-c", "--confidence", type=float, default=0.5,
+                    help="confidence threshold")
+parser.add_argument("-t", "--threshold", type=float, default=0.4,
+                    help="non-maximum supression threshold")
+parser.add_argument("-y", "--yolo", type=str, default="yolov4-tiny",
+                    help="path to yolo directory")
+                    
+args = parser.parse_args()
+logger.info("Parsed Arguments")
