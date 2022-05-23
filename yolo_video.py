b81f6b9f0d007d58b30f3e05829463c407b1b6a9
omar naser said <72103983+omarelshamy12@users.noreply.github.com>
2022-05-23 19:10:12 +0200

    Update yolo_video.py

--- yolo_video.py
+++ yolo_video.py
@@ -1,9 +1,9 @@
+#importing necessary libraries
 import argparse
 import glob
 import time
 import logging
 from pathlib import Path
-
 import cv2
 import numpy as np
 
@@ -16,6 +16,7 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
 parser = argparse.ArgumentParser()
+#defining parser arguments
 parser.add_argument("-i", "--input", type=str, default="",
                     help="path to input video file")
 parser.add_argument("-o", "--output", type=str, default="",
@@ -35,48 +36,65 @@ parser.add_argument("-y", "--yolo", type=str, default="yolov4-tiny",
                    
 args = parser.parse_args()
 logger.info("Parsed Arguments")
-
+#threshold of drawing boxes in the output
 CONFIDENCE_THRESHOLD = args.confidence
+#threshold for handling overlapped boxes by iou then non maximum suppression
 NMS_THRESHOLD = args.threshold
+#if user doesnot enter path of input video file
 if not Path(args.input).exists():
     raise FileNotFoundError("Path to video file is not exist.")
-
+#reading video as an array of frames
 vc = cv2.VideoCapture(args.input)
+#getting the no of frames per second to pass it to function that writes the output file
 fps=vc.get(cv2.CAP_PROP_FPS)
+
+#"yolo-tiny" files path
 if args.yolo=="yolov4-tiny":
     weights ="yolo/yolov4-tiny.weights"
     labels ="yolo/labels.txt"
     cfg ="yolo/yolov4-tiny.cfg"
+#"yolo" files path
 elif args.yolo=="yolov4":
     weights ="yolov4/yolov4.weights"
     labels ="yolov4/labels.txt"
     cfg ="yolov4/yolov4.cfg"
 
-
+#displaying yolo files to user
 logger.info("Using {} weights ,{} configs and {} labels.".format(weights, cfg, labels))
 
+#reading class names
 class_names = list()
 with open(labels, "r") as f:
     class_names = [cname.strip() for cname in f.readlines()]
 
+#Randomly generate the colors(the R value, G value and B value) using np.random.randint for each label(thus the size (len(LABELS), 3). These colors will be used to highlight the detected object of that class later.
 COLORS = np.random.randint(0, 255, size=(len(class_names), 3), dtype="uint8")
 
+#reading parameters of yolo darknet
 net = cv2.dnn.readNetFromDarknet(cfg, weights)
+#prefer to use gpu
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
+#determine only the *output* layer names that we need from YOLO
 layer = net.getLayerNames()
 layer = [layer[i - 1] for i in net.getUnconnectedOutLayers()]
+#initialize the writer
 writer = None
 
+#detect function
 def detect(frm, net, ln):
+    #frame dimensions
     (H, W) = frm.shape[:2]
+    #A binary large object (BLOB or blob) is a collection of binary data stored as a single entity to be an input to darknet
     blob = cv2.dnn.blobFromImage(frm, 1 / 255.0, (416, 416), swapRB=True, crop=False)
     net.setInput(blob)
+    #calculate time of each frame
     start_time = time.time()
+    #forward propagation of frame in the darknet
     layerOutputs = net.forward(ln)
     end_time = time.time()
-
+    
+    #initializing list of boxes,classes and confidences
     boxes = []
     classIds = []
     confidences = []
